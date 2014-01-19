ef7592186934482e105b0d5e43456fdc2060d842
Dublin R <DublinRUserGroup@gmail.com>
2014-01-19 00:38:33 +0000

    Update 06-InvestmentCharts.R

--- 06-InvestmentCharts.R
+++ 06-InvestmentCharts.R
@@ -8,6 +8,11 @@ loadSymbols(c('^XAU', 'GLD', 'IAU'))
 chartSeries(GLD)
 
 ####################################
+library(ggplot2)
+qplot(Quarter, Value, data=melted_gold, fill=Name, geom="bar" ) +
+opts(title='2009-2010 Gold Demand')
+
+####################################
 ggplot(melted_gold, aes(Quarter, Value)) 
     + geom_bar() 
     + facet_wrap(~Name, ncol = 2)
