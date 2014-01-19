a435d6a952504d84fb16c91032e0debacbb9878e
Dublin R <DublinRUserGroup@gmail.com>
2014-01-19 00:38:08 +0000

    Update 06-InvestmentCharts.R

--- 06-InvestmentCharts.R
+++ 06-InvestmentCharts.R
@@ -1,6 +1,18 @@
+# http://www.r-bloggers.com/analyze-gold-demand-and-investments-using-r/
+
+####################################
 library(quantmod)
 loadSymbols(c('^XAU', 'GLD', 'IAU'))
 
 
 chartSeries(GLD)
+
 ####################################
+ggplot(melted_gold, aes(Quarter, Value)) 
+    + geom_bar() 
+    + facet_wrap(~Name, ncol = 2)
+####################################
+ggplot(melted_gold, aes(Quarter, Value, fill=Name)) + 
+    geom_bar(stat="identity") + 
+    facet_wrap(~Name, nrow=4) + 
+    coord_flip()
